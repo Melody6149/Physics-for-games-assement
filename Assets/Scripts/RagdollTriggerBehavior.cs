7d29d0b780d9af68a09db3c43e957bb0c64a6821
Melody <reid.eastin@gmail.com>
2020-09-25 16:35:12 -0500

    updated documentation

---
+++ Assets/Scripts/RagdollTriggerBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class RagdollTriggerBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("ball"))
+                {
+            gameObject.GetComponentInParent<CharacterController>().enabled = false;
+            gameObject.GetComponentInParent<Animator>().enabled = false;
+            
+            
+                }
+    }
+}
---
+++ Assets/Scripts/platformTriggerBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformTriggerBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("ball"))
+        {
+            gameObject.GetComponentInParent<Rigidbody>().isKinematic = false;
+        }
+    }
+}
